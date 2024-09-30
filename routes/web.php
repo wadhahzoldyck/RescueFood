9b392106ef934a1ac448026f8814343314e85c37
wnaggui <wadhah.naggui@esprit.tn>
2024-09-30 22:47:49 +0100

    commit

--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,11 @@ use App\Http\Controllers\RestaurantController;
 |
 */
 
-<<<<<<< HEAD
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/','App\Http\Controllers\TemplateController@index');
 Route::get('/about','App\Http\Controllers\TemplateController@about');
 Route::get('/contact','App\Http\Controllers\TemplateController@contact');
 
-=======
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('/restaurant', [RestaurantController::class, 'index']);
 Route::get('/association/dashboard', function () {
     return view('Associationspace.home');
 })->name('dashboard');
->>>>>>> 00471d5c68083ec6413cfb3f3e76f3f297b8e339
