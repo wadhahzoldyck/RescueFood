596cac2d82517ee1a3b63ddd568ebcc75c1c49c1
Akramhn <akram.hadjnaser@esprit.tn>
2024-10-19 14:30:25 +0100

    integration jointure Recommandation avec nourreture

--- database/migrations/2024_09_30_222340_create_recommandations_table.php
+++ database/migrations/2024_09_30_222340_create_recommandations_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('recommandations', function (Blueprint $table) {
             $table->id(); 
+            $table->foreignId('nourriture_id')->constrained()->onDelete('cascade');
             $table->string('titre'); 
             $table->text('description'); 
             $table->string('categorie'); 
